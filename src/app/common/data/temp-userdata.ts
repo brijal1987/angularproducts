b232a8c3afd67f939218ca8c810002fc624adafa
Paresh <paresh.dalsaniya@datavim.com>
2018-08-12 16:44:56 +0530

    changes

--- src/app/common/data/temp-userdata.ts
+++ src/app/common/data/temp-userdata.ts
@@ -4,6 +4,6 @@ export const Users: User[] = [
     {
         id: 1,
         email: 'brijal.savaliya@gmail.com',
-        password: 'e10adc3949ba59abbe56e057f20f883e'
+        password: 'e10adc3949ba59abbe56e057f20f883e'//123456
     }
 ];
